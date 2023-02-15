0128d9e85c43089783fb285acc2e54a04aebf36b
Amakoye <charlesamakoye@gmail.com>
2023-02-15 14:29:46 +0300

    add about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <Nav />
+      <About />
     </>
   );
 }
---
+++ src/components/about/About.jsx
@@ -0,0 +1,47 @@
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
+import ME from "../../assets/me-about.jpg";
+import "./about.css";
+
+function About() {
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About " />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1+ years working</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>5+ completed</small>
+            </article>
+          </div>
+          <p>
+            I'm a Front End Developer with 1+ years of experience. My current
+            tools of choice include Typescript & Javascript, React & Next, HTML
+            & CSS, Material UI & Tailwind CSS. I'm in constant search of growth
+            opportunities, where I can collaborate with top talent, all
+            motivated by a desire to bring a unique high-end value to everyone I
+            work with.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
